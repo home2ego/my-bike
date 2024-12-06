6152ce071ddb1140c37e29dfe53a4c5861dcb2f1
Artem Zhyrnyi <ic3ego@gmail.com>
2024-12-06 19:40:05 +0200

    install vite-plugin-html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
     build: {
         polyfillModulePreload: false, // Disable the modulepreload polyfill
     },
+    plugins: [
+        createHtmlPlugin({
+            inject: {
+                // Inject the preload link
+                tags: [
+                    {
+                        tag: 'link',
+                        attrs: {
+                            rel: 'preload',
+                            href: '/assets/index-Bvw2nE4X.css',
+                            as: 'style',
+                            onload: "this.onload=null;this.rel='stylesheet'", // Lazy load the stylesheet
+                        },
+                        injectTo: 'head', // Insert it in the <head> section
+                    },
+                ],
+            },
+        }),
+    ],
 });
