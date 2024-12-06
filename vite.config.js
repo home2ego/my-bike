c770bae4c148d8ecd9e9b4054d24ac39c1473d47
Artem Zhyrnyi <ic3ego@gmail.com>
2024-12-06 15:08:43 +0200

    switch to parcel

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import htmlMinifier from 'vite-plugin-html-minifier';
-
-export default defineConfig({
-  plugins: [
-    htmlMinifier(), // Uses default options
-  ],
-});
