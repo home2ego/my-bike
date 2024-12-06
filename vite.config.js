cb22a16c738bb9b2e4a8acef494831c78b5e7d7d
Artem Zhyrnyi <ic3ego@gmail.com>
2024-12-06 20:07:39 +0200

    code refactoring

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,9 @@
 import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
     build: {
         modulePreload: {
             polyfill: false, // Disable the modulepreload polyfill
         },
-        cssCodeSplit: false, // Ensure the CSS isn't split
     },
-    plugins: [
-        createHtmlPlugin({
-            inject: {
-                tags: [
-                    {
-                        tag: 'link',
-                        attrs: {
-                            rel: 'preload',
-                            href: '/assets/index-Bvw2nE4X.css',
-                            as: 'style',
-                            onload: "this.onload=null;this.rel='stylesheet'", // Lazy load the stylesheet
-                        },
-                        injectTo: 'head', // Inject in <head> section
-                    },
-                ],
-            },
-            minify: {
-                removeComments: true, // Minify HTML
-                collapseWhitespace: true,
-                minifyCSS: true,
-                minifyJS: true,
-            },
-        }),
-    ],
 });
